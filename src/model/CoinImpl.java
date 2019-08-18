ece39cba9a8f529d4df053f99d780eb41fb4483e
Soupraa <49212960+Soupraa@users.noreply.github.com>
2019-08-18 20:18:31 +1000

    beginning
    started A1

--- src/model/CoinImpl.java
+++ src/model/CoinImpl.java
@@ -1,5 +1,39 @@
 package model;
 
-public class CoinImpl {
+import model.enumeration.CoinFace;
+import model.interfaces.Coin;
+
+public class CoinImpl implements Coin {
+	private int coin1;
+	private int coin2;
+	
+	public CoinImpl(int coin1, int coin2) {
+		this.coin1 = coin1;
+		this.coin2 = coin2;
+	}
+
+	@Override
+	public int getNumber() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public CoinFace getFace() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void flip() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean equals(Coin coin) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
